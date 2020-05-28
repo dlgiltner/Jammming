95e0cdd8bb82d1efd33694cace63c46fead06e87
dlgiltner <dlgiltner@hotmail.com>
2020-05-28 14:37:26 -0500

    Initial commit

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleKeyPress(e) {
+        if(e.keyCode === 13) {
+            this.search();
+        }
+    }
+
+    handleTermChange(event) {
+        this.setState({term: event.target.value});
+    }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" 
+                    onChange={this.handleTermChange} onKeyDown={this.handleKeyPress} />
+                <button className="SearchButton" onClick={this.search}>SEARCH</button>
+            </div>
+        );
+    }
+};
+
+export default SearchBar;
